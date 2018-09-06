5a2068848b5d538b0e15f2a5cadc3653118db735
Reno Sorensen <renojames19@gmail.com>
2018-09-06 01:53:07 +0000

    Stubbed out BBG:CLI.start functionality

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -3,7 +3,21 @@
 class BestBoardGames::CLI
 
   def self.start
-    puts "The CLI is Running"
+    self.greet
+    self.menu
+    self.goodbye
+  end
+
+  def self.greet
+    puts "Welcome to Best Board Games, your Interface for the top games from BoardGameGeek.com!"
+  end
+
+  def self.menu
+    puts "This will be some data concerning board games."
+  end
+
+  def self.goodbye
+    puts "Thank you for using Best Board Games, we hope to see you again! Game well!"
   end
 
 end
