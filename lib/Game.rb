e86ace6b3d84a5506bfdb266a1c4dc315b3557a5
Reno Sorensen <renojames19@gmail.com>
2018-09-07 00:43:31 +0000

    Created game.rb, required dependency

---
+++ lib/Game.rb
@@ -0,0 +1,13 @@
+
+
+class BestBoardGames::Game
+
+
+
+
+
+
+
+
+
+end
--- lib/best_board_games.rb
+++ lib/best_board_games.rb
@@ -1,5 +1,6 @@
 require_relative "best_board_games/version"
 require "./lib/CLI"
+require "./lib/Game"
 
 module BestBoardGames
   # Your code goes here...
