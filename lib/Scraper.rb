bdcfa531803af00983a80300484c79a4173e8119
Reno Sorensen <renojames19@gmail.com>
2018-09-19 00:11:56 +0000

    Scraped and added game names and page links to their own hashes succesfully

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -16,6 +16,15 @@ class BestBoardGames::Scraper
     # doc.css("#row_") => Grabs each game row
     # doc.css("#row_ .collection_objectname a") => Grabs all game titles
 
+    doc.css("#row_").each do |game|
+      game_hash = {}
+      game_hash[:link] = "https://boardgamegeek.com#{game.css(".collection_objectname a").attribute("href").value}"
+      game_hash[:name] = game.css(".collection_objectname a").text
+
+      @@games_array << game_hash
+
+    end
+
     binding.pry
 
   end
